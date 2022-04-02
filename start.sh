7c356fce1bbac62fed354a35df1f4ce49ff5eca9
Artem Abramov <notdepot@gmail.com>
2022-04-03 00:33:12 +0300

    added mysql logging

--- start.sh
+++ start.sh
@@ -2,4 +2,7 @@ docker build --no-cache -t app ./app/
 #cp -n ./.env.tmp ./.env.dev
 docker-compose --env-file ./.env up -d
 
+docker exec -d mysql chown -R mysql:mysql /var/log/mysql/
+docker restart mysql
+
 #docker exec -d app bash -c "source /app/venv/bin/activate && celery -A app.core.worker.celery worker -Q celery --loglevel=info"
