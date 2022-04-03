3ec0b7825c8a6c8b420299159870550ddd6e1f7c
Artem Abramov <notdepot@gmail.com>
2022-04-03 19:57:35 +0300

    fix

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ docker-compose --env-file ./.env up -d
 docker exec -d mysql chown -R mysql:mysql /var/log/mysql/
 docker restart mysql
 
-#docker exec -d app bash -c "source /app/venv/bin/activate && celery -A app.core.worker.celery worker -Q celery --loglevel=info"
+#docker exec -d app bash -c "source /app/venv/bin/activate && celery -A app.core.worker.celery worker -Q celery --loglevel=error"
