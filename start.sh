7b8a47cfd96bad7704123b73742ea68da4c59590
Artem Abramov <notdepot@gmail.com>
2022-05-03 20:19:09 +0300

    minor improvements

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ docker-compose --env-file ./.env up -d
 docker exec -d mysql chown -R mysql:mysql /var/log/mysql/
 
 #docker exec -d redis chown -R www-data:root /var/log/redis/
-#docker exec -d app bash -c "source /app/venv/bin/activate && celery -A app.core.worker.celery worker -Q celery -f /var/log/celery/celery.log --loglevel=info"
+#docker exec -d app bash -c "source /app/venv/bin/activate && celery -A app.core.app_worker.celery worker -Q celery -f /var/log/celery/celery.log --loglevel=info"
 
 #apt install -y ntp
 #
