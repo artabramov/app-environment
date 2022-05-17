0b6c7cd737975b505530f3d9e0ab9023c939b01b
Art Abramov <notdepot@gmail.com>
2022-05-17 23:06:10 +0300

    stop secondary containers after start

--- start.sh
+++ start.sh
@@ -1,15 +1,16 @@
 #sudo chmod 666 /var/run/docker.sock
-docker build --no-cache -t app ./app/
+#docker build --no-cache -t app ./app/
 #cp -n ./.env.tmp ./.env.dev
 docker-compose --env-file ./.env up -d
 
 docker exec -d mysql chown -R mysql:mysql /var/log/mysql/
 
+docker stop kibana
+docker stop redis-commander
+docker stop flower
+
 #docker exec -d redis chown -R www-data:root /var/log/redis/
 #docker exec -d app bash -c "source /app/venv/bin/activate && celery -A app.core.app_worker.celery worker -Q celery -f /var/log/celery/celery.log --loglevel=info"
-
-#apt install -y ntp
-#
 
 # apt-get update && apt-get upgrade -y && apt-get install -y ntp
 # ntpd -gq
