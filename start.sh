e88af37a7339e5d5f308cbd635389bdde89cc1be
Art Abramov <notdepot@gmail.com>
2022-05-18 22:24:33 +0300

    updated Postman queries

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
-#sudo chmod 666 /var/run/docker.sock
+# docker data: \\wsl$\docker-desktop-data\version-pack-data\community\docker
+
+sudo chmod 666 /var/run/docker.sock
 #docker build --no-cache -t app ./app/
 #cp -n ./.env.tmp ./.env.dev
 docker-compose --env-file ./.env up -d
