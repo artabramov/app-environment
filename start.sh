ab939246bc9bc936b03c52645282793d7e8d179d
Art Abramov <notdepot@gmail.com>
2022-05-22 22:41:10 +0300

    changed logic

--- start.sh
+++ start.sh
@@ -1,22 +1,18 @@
 # docker data: \\wsl$\docker-desktop-data\version-pack-data\community\docker
+# kill vmmem: wsl --shutdown
 
-sudo chmod 666 /var/run/docker.sock
-#docker build --no-cache -t app ./app/
+#sudo chmod 666 /var/run/docker.sock
+docker build --no-cache -t app ./app/
 #cp -n ./.env.tmp ./.env.dev
 docker-compose --env-file ./.env up -d
 
-docker exec -d mysql chown -R mysql:mysql /var/log/mysql/
+#docker exec -d mysql chown -R mysql:mysql /var/log/mysql/
 
-docker stop kibana
-docker stop redis-commander
-docker stop flower
+#docker stop kibana
+#docker stop redis-commander
 
 #docker exec -d redis chown -R www-data:root /var/log/redis/
-#docker exec -d app bash -c "source /app/venv/bin/activate && celery -A app.core.app_worker.celery worker -Q celery -f /var/log/celery/celery.log --loglevel=info"
 
-# apt-get update && apt-get upgrade -y && apt-get install -y ntp
-# ntpd -gq
-# service ntp start
-# echo Europe/Moscow >/etc/timezone && dpkg-reconfigure -f noninteractive tzdata
+
 
 
