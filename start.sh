71e0b28093253bfe9fbad84e42181def6c53cfcf
Art Abramov <notdepot@gmail.com>
2022-06-26 11:48:27 +0300

    updated

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 # run tests (from /app): source /app/venv/bin/activate && python3 -m unittest -v tests.user_model_test
 
 #sudo chmod 666 /var/run/docker.sock
+#docker build --no-cache -t app ./app/
 docker build --no-cache -t app ./app/
 #cp -n ./.env.tmp ./.env.dev
 docker-compose --env-file ./.env up -d
